fd8cf47358337a35c9678a4498ad5db0a0dc43c7
vismayrathore123 <vismayrathore2021@gmail.com>
2024-08-06 18:05:10 +0530

    Added ContentNegotiation in startup.file Added Migration Added DataSeeding in ApplicationDbContext File

--- EComm/Data/ApplicationDbContext.cs
+++ EComm/Data/ApplicationDbContext.cs
@@ -1,15 +1,31 @@
 ﻿using EComm.Models;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
 
 namespace EComm.Data
 {
-    public class ApplicationDbContext:DbContext
+    public class ApplicationDbContext : DbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> Options) : base(Options)
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
-            
         }
+
         public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category
+                {
+                    Id = 1,
+                    Title = "Samsung",
+                    DisplayOrder = 1,
+                },
+                new Category
+                {
+                    Id = 2,
+                    Title = "Realme",
+                    DisplayOrder = 2
+                });
+        }
     }
 }
